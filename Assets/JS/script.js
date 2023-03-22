0716eb696a1ea871eda69859f525856d63d96226
zeinahares <zeinahares@gmail.com>
2023-03-22 22:51:25 +1100

    tweaked html body, finished my function for fetching the list of movies

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,14 +1,18 @@
 // Code Questions:
 // CSS framework? choose one and delete the other from html 
 
+// TO RAISE:
+// NEED AN EXTRA FUNCTION FOR WHAT HAPPENS WHEN WE PRESS THE PRINTED the printed search history
 
 // OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=d673ee57
 
 var OMDbAPIkey = 'd673ee57';
+var OMDbAPIParameter = '&apikey=d673ee57'
+var baseURLOMDb = 'http://www.omdbapi.com/?'
 
-var requestUrl = 'http://www.omdbapi.com/?i=tt1201607&apikey=d673ee57'; // t = specific title , s = search title
+var ExamplerequestUrl = 'http://www.omdbapi.com/?i=tt1201607&apikey=d673ee57'; // t = specific title , s = search title, i = id of specific title
 
-fetch(requestUrl)
+fetch(ExamplerequestUrl)
   .then(function (response) {
     return response.json();
   })
@@ -21,9 +25,91 @@ fetch(requestUrl)
 //  CSS - TOM
 //  README - TOM
 
-// ZEINA - search function advent listener for
+// ZEINA - function advent listener for searching movies
 // take text from input button + create request URL
-// display the list of movie titles // appending
+// display the list of movie titles
+
+function fetchmoviesList(event) {
+
+  event.preventDefault();
+  var movieInput = $('#search').val();
+
+  if (movieInput.length === 0) {
+    return
+  }
+
+  console.log(movieInput);
+
+  var movieQuery = movieInput.replace(/ /g, '+');
+
+  console.log(movieQuery);
+
+  var movieListrequestURL = baseURLOMDb + 's=' + movieQuery + OMDbAPIParameter;
+
+  // remove all inner html of the divs on new search
+  $('#movieslist').html("");
+  $('#moviepage').html("");
+
+  fetch(movieListrequestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      // remove hide attribute from movieslist & hide moviepage
+      $('#movieslist').removeClass();
+      $('#moviepage').addClass('hide');
+
+      for (i = 0; i < data.Search.length; i++) {
+
+        var posterURL = data.Search[i].Poster;
+        console.log(posterURL);
+        var movieTitle = data.Search[i].Title;
+        var movieYear = data.Search[i].Year;
+
+        // create a div with .card
+
+        var newCard = $("<div class= 'card'>");
+
+        // append newCard to row #movieslist
+        $('#movieslist').append(newCard);
+
+        // create a title with title & year & append child to div newCard
+
+        var movieTitleEl = $("<span class='card-title'>");
+
+        movieTitleEl.text(movieTitle + ' (' + movieYear + ')');
+
+        newCard.append(movieTitleEl);
+
+        // create img with poster URL & append child to div newCard
+
+        var movieposterEl = $('<img>');
+        movieposterEl.attr('src', posterURL);
+
+        newCard.append(movieposterEl);
+      };
+
+      $('#search').val('');
+    });
+
+}
+
+$('#search').on('submit', fetchmoviesList);
+
+$('#search').on('keypress', function (event) {
+
+  // if user presses enter on the page, fetch the movies list
+
+  if (event.keyCode === 13) {
+    console.log(event.keyCode);
+    fetchmoviesList(event);
+  }
+});
+
+
+
 
 // SAWAKO = fuction save to local storage from search history
 
